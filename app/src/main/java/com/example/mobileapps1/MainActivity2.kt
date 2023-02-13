a6145a07fbd57a7012033770ebfc334f0a69d422
KLayton Neves <klayton1392@gmail.com>
2023-02-13 14:23:34 +0000

    Activity LifeCycle

--- app/src/main/java/com/example/mobileapps1/MainActivity2.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity2.kt
@@ -8,4 +8,25 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
     }
+
+    override fun onPause(){
+
+        super.onPause()
+        Log.i("main activity", "hi there")
+
+    }
+
+    override fun onResume(){
+
+        super.onResume()
+        Log.e("main activity", "this activity has been resumed")
+
+    }
+
+    override fun onDestroy(){
+
+        super.onDestroy()
+        Log.e"main activity", "this activity has been destroyed")
+
+    }
 }
